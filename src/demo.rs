52868e48a88a64826d536dec6e13269938977138
Hannu Hartikainen <hannu.hartikainen@gmail.com>
2018-03-15 19:38:58 +0200

    simple looping demo

--- src/demo.rs
+++ src/demo.rs
@@ -6,7 +6,20 @@ pub fn rust_logo() -> [u8; 504] {
 }
 
 pub fn demo(pcd8544: &mut Pcd8544) {
-    let logo = rust_logo();
-    pcd8544.draw_buffer(&logo);
+    loop {
+        let mut buffer = rust_logo();
+        pcd8544.draw_buffer(&buffer);
+        for i in 0..84 {
+            for j in 0..6 {
+                buffer[6*i+j] = 0xFF;
+            }
+            pcd8544.draw_buffer(&buffer);
+        }
+        for i in (0..84).rev() {
+            for j in 0..6 {
+                buffer[6*i+j] = 0x00;
+            }
+            pcd8544.draw_buffer(&buffer);
+        }
+    }
 }
-
